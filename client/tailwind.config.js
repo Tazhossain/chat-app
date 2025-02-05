2250d18733a7d61e635c0143c4605ca8f5f7628c
Taz <43081043+Tazhossain@users.noreply.github.com>
2025-02-06 00:59:36 +0600

    Delete client/tailwind.config.js

--- client/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: ["class"],
-  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx,jsx,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("tailwindcss-animate")],
-}
